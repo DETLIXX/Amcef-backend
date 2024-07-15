701740182c1ebfd47d319782e5a7e492ed8060c8
DETLIXX <dr.detlix@gmail.com>
2024-07-15 16:27:23 +0200

    Commit

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,30 +6,16 @@ import { LoginDTO } from 'src/dto/auth.dto';
 import { Member } from 'src/schemas/Members.schema';
 import { HashService } from '../../common/auth.common.service';
 import { CreateMemberDTO } from 'src/dto/members.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(Member.name) private memberModel: Model<Member>,
     private readonly hashService: HashService,
+    private jwtService: JwtService,
   ) {}
-  async login(body: LoginDTO): Promise<object> {
-    console.log(body);
-    const { email, password } = body;
 
-    const member = await this.memberModel.findOne({ email }).lean().exec();
-
-    if (!member) throw new NotFoundException('Member not found');
-
-    const isPasswordValid = await this.hashService.comparePassword(
-      password,
-      member.password,
-    );
-
-    if (!isPasswordValid) throw new NotFoundException('Password is not valid');
-
-    return null; // TODO: ZMENIT
-  }
   async register(body: CreateMemberDTO): Promise<Member> {
     const { username, email, password } = body;
     const returnHash = await this.hashService.hashPassword(password);
